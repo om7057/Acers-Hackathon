131cf9194ee35c2ed2f223d132b39f4f1736e8e1
OM <kulkarniom7057@gmail.com>
2023-03-16 16:25:51 +0530

    Body Parsing

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,25 @@
 const express=require('express');
 const app=express();
-const morgan=require('morgan')
+const morgan=require('morgan');
+const bodyParser=require('body-parser');
 
 const productRoutes=require('./api/routes/products');
 const orderRoutes=require('./api/routes/orders');
 
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use((req,res,next)=>{
+    res.header('Access-Contol-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Origin,X-Requested-with,Content-TypeError,Accept,Authorization');
+    if(req.method==='OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT,POST,GET,DELETE,PATCH');
+        res.status(200).json({});
+    }
+    next();
+});
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 app.use((req,res,next)=>{
