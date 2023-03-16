d5c94273cbdb529cc13b18b82268166799ba00af
OM <kulkarniom7057@gmail.com>
2023-03-16 14:14:21 +0530

    commit

--- Backend/api/routes/products.js
+++ Backend/api/routes/products.js
@@ -13,4 +13,30 @@ router.post('/',(req,res,next)=>{
     });
 });
 
+router.get('/:productId',(req,res,next)=>{
+    const id=req.params.productId; 
+    if(id==='special'){
+  res.status(200).json({
+    message:'s',
+    id:id
+  })
+    }
+    else{
+        res.status(200).json({
+            message:'you passed id'
+        });
+    }
+});
+
+router.patch('/:productId',(req,res,next)=>{
+    res.status(200).json({
+        message:'Product Updated!!'
+    }); 
+});
+
+router.delete('/:productId',(req,res,next)=>{
+    res.status(200).json({
+        message:'Product Deleted!!'
+    });
+});
 module.exports=router;
